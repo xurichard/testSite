919860e51189be6a9f9d0a8354c4139d60cc75e0
xurichard <richardxu@berkeley.edu>
2015-09-02 01:18:23 -0700

    This is a mess, I'll clean it later

---
+++ mail/contact_me.php
@@ -0,0 +1,95 @@
+<?php
+//checking for empty fields
+if(empty($_POST['name'])      ||
+   empty($_POST['email'])     )
+   {
+   echo "No arguments Provided!";
+   return false;
+   }
+   
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+//SMTP needs accurate times, and the PHP time zone MUST be set
+//This should be done in your php.ini, but this is how to do it if you don't have access to that
+date_default_timezone_set('Etc/UTC');
+
+require 'PHPMailerAutoload.php';
+
+//Create a new PHPMailer instance
+$mail = new PHPMailer;
+
+//Tell PHPMailer to use SMTP
+$mail->isSMTP();
+
+//Enable SMTP debugging
+// 0 = off (for production use)
+// 1 = client messages
+// 2 = client and server messages
+$mail->SMTPDebug = 0;
+
+//Ask for HTML-friendly debug output
+$mail->Debugoutput = 'html';
+
+//Set the hostname of the mail server
+$mail->Host = 'smtp.gmail.com';
+
+//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+$mail->Port = 587;
+
+//Set the encryption system to use - ssl (deprecated) or tls
+$mail->SMTPSecure = 'tls';
+
+//Whether to use SMTP authentication
+$mail->SMTPAuth = true;
+
+//Username to use for SMTP authentication - use full email address for gmail
+$mail->Username = "richardxu1996@gmail.com";
+
+//Password to use for SMTP authentication
+$mail->Password = "PassWord42";
+
+//Set who the message is to be sent from
+$mail->setFrom($email_address, $name);
+
+//Set an alternative reply-to address
+//$mail->addReplyTo('replyto@example.com', 'First Last');
+
+//Set who the message is to be sent to
+$mail->addAddress('richardxu1996@gmail.com', 'Richard Xu');
+
+//Set the subject line
+$mail->Subject = 'Personal Website message';
+
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+//$mail->msgHTML(file_get_contents('test.html'), dirname(__FILE__));
+
+$mail->Body = "name: ".$name."<br>"."email address: ".$email_address."<br>"."phone: ".$phone. "<br>". $message;
+
+//Replace the plain text body with one created manually
+$mail->AltBody = 'This is a plain-text message body';
+
+//Attach an image file
+//$mail->addAttachment('images/phpmailer_mini.png');
+
+//send the message, check for errors
+if (!$mail->send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    echo "Message sent!";
+}
+
+/*
+echo $name;
+echo $email_address;
+echo $phone;
+echo $message;
+*/
+
+header('Location: http://richardxu.me/');
+exit;
+
+?>
